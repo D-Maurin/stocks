1459cab99ce4f8d9d66c2befc7de045afcff9960
Denis Maurin <maurin.denis.m@gmail.com>
2022-03-01 16:56:27 +0100

    dbinit

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import os
+
+database_url = os.getenv('DATABASE_URL')
+print(database_url)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/')
 def index():
+    print(User.query.all())
     return 'hello, world'
