3fe82bd20ba7feb825c0268986ee7ab53450c9e5
Denis Maurin <maurin.denis.m@gmail.com>
2022-03-13 16:50:26 +0100

    serve ui

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ database_url = os.getenv('DATABASE_URL')
 if database_url and database_url.startswith("postgres://"):
     database_url = database_url.replace("postgres://", "postgresql://", 1)
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder='ui/build')
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,3 +22,13 @@ migrate = Migrate(app, db)
 
 app.register_blueprint(itemsAPI, url_prefix='/api')
 app.register_blueprint(itemCategoriesAPI, url_prefix='/api')
+
+
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
